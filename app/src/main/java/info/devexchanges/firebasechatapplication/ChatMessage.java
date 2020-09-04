2c034851959274f7ddd6f3baff20515d39e9989d
Akhil Kumar <akhilskumar1999@gmail.com>
2020-09-04 18:12:17 +0530

    Update ChatMessage.java

--- app/src/main/java/info/devexchanges/firebasechatapplication/ChatMessage.java
+++ app/src/main/java/info/devexchanges/firebasechatapplication/ChatMessage.java
@@ -1,29 +1,23 @@
 package info.devexchanges.firebasechatapplication;
+
 import java.util.Date;
 
 public class ChatMessage {
+
     private String messageText;
     private String messageUser;
-    private String messageUserId;
     private long messageTime;
 
-    public ChatMessage(String messageText, String messageUser, String messageUserId) {
+    public ChatMessage(String messageText, String messageUser) {
         this.messageText = messageText;
         this.messageUser = messageUser;
+
+        // Initialize to current time
         messageTime = new Date().getTime();
-        this.messageUserId = messageUserId;
     }
 
     public ChatMessage(){
 
-    }
-
-    public String getMessageUserId() {
-        return messageUserId;
-    }
-
-    public void setMessageUserId(String messageUserId) {
-        this.messageUserId = messageUserId;
     }
 
     public String getMessageText() {

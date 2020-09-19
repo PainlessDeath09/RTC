7ad502fddcdfe10a66a41bf43648edc1c7fa06da
Akhil Kumar <akhilskumar1999@gmail.com>
2020-09-20 04:05:55 +0530

    Image upload implemention Final

--- app/src/main/java/com/example/rtc/chatScreen.java
+++ app/src/main/java/com/example/rtc/chatScreen.java
@@ -3,6 +3,7 @@ package com.example.rtc;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -17,7 +18,10 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Scroller;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,15 +30,22 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.UUID;
 
 public class chatScreen extends AppCompatActivity {
 
     private DatabaseReference myDatabase;
     private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-    FirebaseStorage storage = FirebaseStorage.getInstance();
+    FirebaseStorage storage = FirebaseStorage.getInstance();;
+    StorageReference storageReference = storage.getReference();;
+
+
     private final int PICK_IMAGE_REQUEST = 71;
     private Uri filePath;
 
@@ -53,13 +64,20 @@ public class chatScreen extends AppCompatActivity {
         myDatabase = FirebaseDatabase.getInstance().getReference("Message");
 
         final TextView myText = findViewById(R.id.textView2);
-
+        Button upload = findViewById(R.id.Upload);
         ImageButton btnChoose = findViewById(R.id.Choose);
 
         btnChoose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 chooseImage();
+            }
+        });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
             }
         });
 
@@ -147,6 +165,8 @@ public class chatScreen extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         ImageView imageView = (ImageView) findViewById(R.id.imgView);
+        Button cancel = findViewById(R.id.Cancel);
+        Button upload = findViewById(R.id.Upload);
 
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
@@ -156,6 +176,10 @@ public class chatScreen extends AppCompatActivity {
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
                 imageView.setImageBitmap(bitmap);
+                imageView.setVisibility(View.VISIBLE);
+                cancel.setVisibility(View.VISIBLE);
+                upload.setVisibility(View.VISIBLE);
+
             }
             catch (IOException e)
             {
@@ -176,4 +200,49 @@ public class chatScreen extends AppCompatActivity {
         //myDatabase.child(username).setValue(textbox.getText().toString());
         textbox.setText("");
     }
+
+    private void uploadImage() {
+
+        final ImageView imageView = (ImageView) findViewById(R.id.imgView);
+        final Button cancel = findViewById(R.id.Cancel);
+        final Button upload = findViewById(R.id.Upload);
+        final Date date = new Date();
+        username = user.getUid();
+        if(filePath != null)
+        {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading...");
+            progressDialog.show();
+
+            StorageReference ref = storageReference.child("images/"+ UUID.randomUUID().toString());
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(chatScreen.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(chatScreen.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0*taskSnapshot.getBytesTransferred()/taskSnapshot
+                                    .getTotalByteCount());
+                            progressDialog.setMessage("Uploaded "+(int)progress+"%");
+                            imageView.setVisibility(View.GONE);
+                            cancel.setVisibility(View.GONE);
+                            upload.setVisibility(View.GONE);
+                            myDatabase.child(username+"__"+Long.toString(date.getTime())).setValue("<<IMAGE>>");
+                        }
+                    });
+        }
+    }
+
 }
